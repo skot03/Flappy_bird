462581efb8ae73dbf68c18fe2e2a064b7796f76e
skot03 <skotnickipawe@gmail.com>
2024-05-09 21:22:48 +0200

    bacground working

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 pygame.init()
 window_width = 864
 window_height = 800
+# got from someone GitHub
 background = pygame.image.load("bg.png")
 pygame.display.set_caption("flappy bird game")
 window = pygame.display.set_mode((window_width, window_height))
@@ -12,6 +13,7 @@ is_game_running = True
 while is_game_running:
     window.blit(background, (0, 0))
     for event in pygame.event.get():
+        # to check if quit pressed
         if event.type == pygame.QUIT:
             is_game_running = False
     pygame.display.update()
